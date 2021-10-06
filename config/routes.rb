7ac9c21f0b05af4fd2ec9384aa763ca12fcc39ba
Alejalons <alejandro.morales@ltec.cl>
2021-10-06 16:41:27 -0300

    no funciona destroy, se marea

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :friends
   # get 'home/index'
+  get 'home/about'
   root 'home#index'
+  delete 'friends/:id', to: "friends#destroy", as: "friends_destroy"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
